59ac0f9c6bbdae77c1d4e81d99d27bedbd0887fd
unknown <rjabogadosasescoms@gmail.com>
2026-06-16 12:42:20 -0500

    Corrigiendo conflicto de rutas estaticas

--- nucleo_rj/settings.py
+++ nucleo_rj/settings.py
@@ -91,7 +91,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'staticfiles'), # Aquí buscará tus diseños
+    os.path.join(BASE_DIR, 'static')
 ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
