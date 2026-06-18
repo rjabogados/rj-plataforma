b5d6e0992d5ca1b508d9a86953c5d4f9774e19bc
unknown <rjabogadosasescoms@gmail.com>
2026-06-18 08:54:47 -0500

    Nuevas rutas dinamicas para los PDFs 2

--- nucleo_rj/urls.py
+++ nucleo_rj/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic import RedirectView # <- 1. Importamos el redireccionador
 from intranet.views.webhook import recibir_matriz_excel
 
 # --- PERSONALIZACIÓN DEL PANEL CORPORATIVO ---
@@ -11,6 +12,9 @@ admin.site.index_title = "Bienvenido al Sistema Central"
 # ---------------------------------------------
 
 urlpatterns = [
+    # 2. La nueva ruta: Si alguien entra sin nada al final, lo mandamos al panel
+    path('', RedirectView.as_view(url='/admin/', permanent=False)), 
+    
     path('admin/', admin.site.urls),
     path('api/webhook/', recibir_matriz_excel, name='webhook_matriz'),
 ]
